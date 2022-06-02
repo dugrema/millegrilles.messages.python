77aaaf7ccc7f62c0f7162582932ff2356f3a22ac
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-06-02 06:44:35 -0400

    Renouvellement certificat instance protege

--- millegrilles_messages/certificats/CertificatsInstance.py
+++ millegrilles_messages/certificats/CertificatsInstance.py
@@ -4,9 +4,10 @@ from millegrilles_messages.certificats.Generes import EnveloppeCsr
 from millegrilles_messages.messages.CleCertificat import CleCertificat
 from millegrilles_messages.messages import Constantes
 from millegrilles_messages.certificats.Generes import ajouter_exchanges, ajouter_roles
+from millegrilles_messages.certificats.Generes import DUREE_CERT_DEFAUT
 
 
-def signer_instance_protege(cle_intermediaire: CleCertificat, csr_pem: str):
+def signer_instance_protege(cle_intermediaire: CleCertificat, csr_pem: str, duree=DUREE_CERT_DEFAUT):
     enveloppe_csr = EnveloppeCsr.from_str(csr_pem)
     builder = CertificateBuilder()
 
@@ -16,6 +17,6 @@ def signer_instance_protege(cle_intermediaire: CleCertificat, csr_pem: str):
     role = 'instance'
     builder = ajouter_roles(builder, role)
 
-    enveloppe_certificat = enveloppe_csr.signer(cle_intermediaire, role, builder=builder)
+    enveloppe_certificat = enveloppe_csr.signer(cle_intermediaire, role, builder=builder, duree=duree)
 
     return enveloppe_certificat
