1daf197491183100bbe65de69211848b4a76bedf
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-06-09 11:02:59 -0400

    Support signature instance privee
    Renouvellement

--- millegrilles_messages/certificats/CertificatsInstance.py
+++ millegrilles_messages/certificats/CertificatsInstance.py
@@ -20,3 +20,33 @@ def signer_instance_protege(cle_intermediaire: CleCertificat, csr_pem: str, dure
     enveloppe_certificat = enveloppe_csr.signer(cle_intermediaire, role, builder=builder, duree=duree)
 
     return enveloppe_certificat
+
+
+def signer_instance_prive(cle_intermediaire: CleCertificat, csr_pem: str, duree=DUREE_CERT_DEFAUT):
+    enveloppe_csr = EnveloppeCsr.from_str(csr_pem)
+    builder = CertificateBuilder()
+
+    exchanges = [Constantes.SECURITE_PRIVE, Constantes.SECURITE_PUBLIC]
+    builder = ajouter_exchanges(builder, exchanges)
+
+    role = 'instance'
+    builder = ajouter_roles(builder, role)
+
+    enveloppe_certificat = enveloppe_csr.signer(cle_intermediaire, role, builder=builder, duree=duree)
+
+    return enveloppe_certificat
+
+
+def signer_instance_public(cle_intermediaire: CleCertificat, csr_pem: str, duree=DUREE_CERT_DEFAUT):
+    enveloppe_csr = EnveloppeCsr.from_str(csr_pem)
+    builder = CertificateBuilder()
+
+    exchanges = [Constantes.SECURITE_PUBLIC]
+    builder = ajouter_exchanges(builder, exchanges)
+
+    role = 'instance'
+    builder = ajouter_roles(builder, role)
+
+    enveloppe_certificat = enveloppe_csr.signer(cle_intermediaire, role, builder=builder, duree=duree)
+
+    return enveloppe_certificat
