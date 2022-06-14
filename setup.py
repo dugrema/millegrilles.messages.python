8d20be1e1147624e52789d260381bb3591a3f793
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-06-14 08:11:49 -0400

    Maj version messages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='millegrilles_messages',
-    version='2022.3.1',
+    version='2022.3.2',
     packages=setuptools.find_packages(),
     url='https://github.com/dugrema/millegrilles.messages.python',
     license='AFFERO',
