7ce99054368a53dac7f04aece231aeb268c3cfdb
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-09-22 14:24:55 -0400

    Fix installation instance secure

--- millegrilles_messages/certificats/CertificatsInstance.py
+++ millegrilles_messages/certificats/CertificatsInstance.py
@@ -7,6 +7,21 @@ from millegrilles_messages.certificats.Generes import ajouter_exchanges, ajouter
 from millegrilles_messages.certificats.Generes import DUREE_CERT_DEFAUT
 
 
+def signer_instance_secure(cle_intermediaire: CleCertificat, csr_pem: str, duree=DUREE_CERT_DEFAUT):
+    enveloppe_csr = EnveloppeCsr.from_str(csr_pem)
+    builder = CertificateBuilder()
+
+    exchanges = [Constantes.SECURITE_SECURE, Constantes.SECURITE_PROTEGE, Constantes.SECURITE_PRIVE, Constantes.SECURITE_PUBLIC]
+    builder = ajouter_exchanges(builder, exchanges)
+
+    role = 'instance'
+    builder = ajouter_roles(builder, role)
+
+    enveloppe_certificat = enveloppe_csr.signer(cle_intermediaire, role, builder=builder, duree=duree)
+
+    return enveloppe_certificat
+
+
 def signer_instance_protege(cle_intermediaire: CleCertificat, csr_pem: str, duree=DUREE_CERT_DEFAUT):
     enveloppe_csr = EnveloppeCsr.from_str(csr_pem)
     builder = CertificateBuilder()
