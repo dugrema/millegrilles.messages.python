4a3fe84e857314984f4da94f5716309d4ba652b9
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-11-15 08:36:53 -0500

    Ajout req aiohttp
    midcompte 2022.8.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='millegrilles_messages',
-    version='2022.8.2',
+    version='2022.8.3',
     packages=setuptools.find_packages(),
     url='https://github.com/dugrema/millegrilles.messages.python',
     license='AFFERO',
