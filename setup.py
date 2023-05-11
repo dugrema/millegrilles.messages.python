2f665a3939d52778fdac2a51f0299a10230bc147
mathieu <mathieu.dugre@mdugre.info>
2023-05-11 08:02:39 -0400

    version 2023.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='millegrilles_messages',
-    version='2023.5.4',
+    version='2023.6.0',
     packages=setuptools.find_packages(),
     url='https://github.com/dugrema/millegrilles.messages.python',
     license='AFFERO',
