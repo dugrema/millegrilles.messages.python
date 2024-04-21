6915655c90de0c298ac30ebac354f1963cf63de4
mathieu <mathieu.dugre@mdugre.info>
2024-04-21 12:24:56 -0400

    Ajout dechiffrer_document_secrete
    Utilise pour indexation

--- millegrilles_messages/chiffrage/DechiffrageUtils.py
+++ millegrilles_messages/chiffrage/DechiffrageUtils.py
@@ -21,6 +21,24 @@ def dechiffrer_document(clecert: CleCertificat, cle_secrete: str, document_chiff
     return contenu_json
 
 
+def dechiffrer_document_secrete(cle_secrete: bytes, document_chiffre: dict):
+    decipher = get_decipher_cle_secrete(cle_secrete, document_chiffre)
+
+    contenu_chiffre = document_chiffre['data_chiffre']
+    if isinstance(contenu_chiffre, str):
+        if document_chiffre.get('nonce'):
+            # Nouveau format, ajouter 'm' pour multibase
+            contenu_chiffre = 'm' + contenu_chiffre
+        contenu_chiffre = multibase.decode(contenu_chiffre)
+
+    contenu_dechiffre = decipher.update(contenu_chiffre)
+    contenu_dechiffre += decipher.finalize()
+
+    contenu_json = json.loads(contenu_dechiffre)
+
+    return contenu_json
+
+
 def get_decipher(clecert: CleCertificat, cle_secrete: str, document_chiffre: dict):
     doc_chiffre_info = document_chiffre.copy()
     doc_chiffre_info['cle'] = cle_secrete
@@ -37,7 +55,11 @@ def get_decipher(clecert: CleCertificat, cle_secrete: str, document_chiffre: dic
 def get_decipher_cle_secrete(cle_secrete: bytes, info_dechiffrage):
     format_chiffrage = info_dechiffrage['format']
     if format_chiffrage == 'mgs4':
-        nonce = info_dechiffrage['nonce']
+        try:
+            nonce = 'm' + info_dechiffrage['nonce']
+        except KeyError:
+            # Ancien format
+            nonce = info_dechiffrage['header']
         decipher = DecipherMgs4(cle_secrete, nonce)
     else:
         raise Exception("Format de chiffrage %s non supporte" % format_chiffrage)
