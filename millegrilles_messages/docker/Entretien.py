cc879181c8542d5ea2f23d4fe05205fe1817437d
mathieu <mathieu.dugre@mdugre.info>
2024-04-29 09:35:31 -0400

    Ajout retry sur tache avec erreur

--- millegrilles_messages/docker/Entretien.py
+++ millegrilles_messages/docker/Entretien.py
@@ -9,9 +9,11 @@ class TacheEntretien:
     def __init__(self, intervalle: datetime.timedelta, callback, get_producer=None):
         self.__logger = logging.getLogger(__name__ + '.' + self.__class__.__name__)
         self.__intervalle = intervalle
+        self.__intervalle_retry = datetime.timedelta(seconds=30)
         self.__callback = callback
         self.__dernier_entretien: Optional[datetime.datetime] = None
         self.__get_producer = get_producer
+        self.__en_erreur = False
 
     def reset(self):
         """
@@ -23,20 +25,28 @@ class TacheEntretien:
     def set_intervalle(self, intervalle: datetime.timedelta):
         self.__intervalle = intervalle
 
+    def set_intervalle_retry(self, intervalle: datetime.timedelta):
+        self.__intervalle_retry = intervalle
+
     async def run(self):
+        now = datetime.datetime.utcnow()
         if self.__dernier_entretien is None:
             pass
-        elif datetime.datetime.utcnow() - self.__intervalle > self.__dernier_entretien:
+        elif self.__en_erreur and now - self.__intervalle_retry > self.__dernier_entretien:
+            pass
+        elif now - self.__intervalle > self.__dernier_entretien:
             pass
         else:
             return
 
-        self.__dernier_entretien = datetime.datetime.utcnow()
+        self.__dernier_entretien = now
 
         try:
             if self.__get_producer is not None:
                 await self.__callback(self.__get_producer)
             else:
                 await self.__callback()
+            self.__en_erreur = False
         except:
             self.__logger.exception("Erreur execution tache entretien")
+            self.__en_erreur = True
