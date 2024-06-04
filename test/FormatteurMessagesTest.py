a4e6feea07721cf0b904014b3e6d8a41476ddf9c
mathieu <mathieu.dugre@mdugre.info>
2024-06-04 14:42:19 -0400

    Fix test formatteur messages

--- test/FormatteurMessagesTest.py
+++ test/FormatteurMessagesTest.py
@@ -1,11 +1,14 @@
 import json
 import logging
 
+from millegrilles_messages.messages import Constantes
 from millegrilles_messages.messages.CleCertificat import CleCertificat
 from millegrilles_messages.messages.FormatteurMessages import SignateurTransactionSimple, FormatteurMessageMilleGrilles
+from millegrilles_messages.messages.EnveloppeCertificat import EnveloppeCertificat
 
-PATH_CORE_CERT = '/home/mathieu/mgdev/certs/pki.core.cert'
-PATH_CORE_CLE = '/home/mathieu/mgdev/certs/pki.core.key'
+PATH_CORE_CERT = '/var/opt/millegrilles/secrets/pki.core.cert'
+PATH_CORE_CLE = '/var/opt/millegrilles/secrets/pki.core.cle'
+PATH_CORE_CA = '/var/opt/millegrilles/configuration/pki.millegrille.cert'
 
 logger = logging.getLogger(__name__)
 
@@ -15,11 +18,13 @@ def formatter_message():
     enveloppe = clecert.enveloppe
     idmg = enveloppe.idmg
 
+    ca = EnveloppeCertificat.from_file(PATH_CORE_CA)
+
     signateur = SignateurTransactionSimple(clecert)
-    formatteur = FormatteurMessageMilleGrilles(idmg, signateur)
+    formatteur = FormatteurMessageMilleGrilles(idmg, signateur, ca)
 
     message_1 = {'valeur': 1, 'texte': 'Du texte.'}
-    message_1_signe, uuid_transaction = formatteur.signer_message(message_1)
+    message_1_signe, uuid_transaction = formatteur.signer_message(Constantes.KIND_DOCUMENT, message_1)
     message_1_indent = json.dumps(message_1_signe, indent=2)
     logger.debug("Message 1 signe (uuid: %s) : %s" % (uuid_transaction, message_1_indent))
 
